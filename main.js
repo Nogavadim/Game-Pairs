549fb328a25ade30865ce4d7cff061b221368f14
Vadim Gaisarov <vadimgajsar@gmail.com>
2023-07-20 22:12:56 +0300

    Load Game Pairs

---
+++ main.js
@@ -0,0 +1,78 @@
+import Card from './card.js'
+
+
+
+
+function newGame(container, cardsCount) {
+
+  //  создать поле
+   let cardsNumberArray = [],
+    cardsArray = [],
+    firstCard = null,
+    secondCard = null;
+
+  for (let i = 1; i <= cardsCount / 2; i++) {
+    cardsNumberArray.push(i)
+    cardsNumberArray.push(i)
+  }
+
+  cardsNumberArray = cardsNumberArray.sort(() => Math.random() - 0.5);
+
+  for (const cardNumber of cardsNumberArray) {
+    cardsArray.push(new Card(container, cardNumber, flip))
+  }
+
+
+  // логика игры
+  function flip(card) {
+
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number != secondCard.number) {
+        firstCard.open = false
+        secondCard.open = false
+        firstCard = null
+        secondCard = null
+      }
+    }
+
+
+    if (firstCard == null) {
+      firstCard = card
+    } else {
+      if (secondCard == null) {
+        secondCard = card
+      }
+    }
+
+    if (firstCard !== null && secondCard !== null) {
+      if (firstCard.number == secondCard.number) {
+        firstCard.success = true
+        secondCard.success = true
+        firstCard = null
+        secondCard = null
+      }
+    }
+
+
+    if (document.querySelectorAll('.card.success').length == cardsNumberArray.length) {
+      // сброс игры
+      alert('Победа!!! игра закончена')
+
+      container.innerHTML = ''
+      cardsNumberArray = []
+    cardsArray = []
+    firstCard = null
+    secondCard = null
+
+    newGame(container, cardsCount)
+    }
+  }
+
+
+}
+
+
+newGame(document.getElementById('game'), 16)
+
+
+
